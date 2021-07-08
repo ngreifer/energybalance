d585b2389448c0c8459063931b3f5b1b0e57d538
Noah Greifer <noah.greifer@gmail.com>
2021-07-08 12:28:24 -0400

    Added lambda argument to control square of weights

--- R/eb_wrappers.R
+++ R/eb_wrappers.R
@@ -1,19 +1,26 @@
-eb_ate <- function(sampleX, Z, sampleW = NULL, std = "studentized", improved = TRUE) {
-  energybalance(sampleX, Z = Z, sampleW = sampleW, std = std, improved = improved)
+eb_ate <- function(sampleX, Z, sampleW = NULL, std = "studentized", improved = TRUE, lambda = 0) {
+  energybalance(sampleX, Z = Z, sampleW = sampleW, std = std, improved = improved,
+                lambda = lambda)
 }
 
-eb_target <- function(sampleX, targetX, sampleW = NULL, targetW = NULL, std = "studentized") {
-  energybalance(sampleX, targetX = targetX, sampleW = sampleW, targetW = targetW, std = std)
+eb_target <- function(sampleX, targetX, sampleW = NULL, targetW = NULL, std = "studentized", lambda = 0) {
+  energybalance(sampleX, targetX = targetX, sampleW = sampleW, targetW = targetW,
+                std = std, lambda = lambda)
 }
 
-eb_mediation <- function(sampleX, Z, M, sampleW = NULL, std = "studentized") {
-  sampleWc <- process_w(sampleW, Z)[Z==0]
+eb_mediation <- function(sampleX, Z, M, sampleW = NULL, std = "studentized", lambda = 0) {
+  Z <- process_Z(Z, bin = TRUE)
+  M <- process_Z(M)
 
-  w_ate_c <- eb_target(sampleX[Z==0,], targetX = sampleX, sampleW = sampleWc,
-                       targetW = sampleWc, std = std)
+  sampleW <- process_w(sampleW, Z)
+
+  check_lengths(Z, sampleX, M, sampleW)
+
+  w_ate_c <- eb_target(sampleX[Z==0,], targetX = sampleX, sampleW = sampleW[Z==0],
+                       targetW = sampleW, std = std, lambda = lambda)
 
   sampleXM <- cbind(sampleX, M)
 
-  eb_target(sampleXM[Z==1,], targetX = sampleXM[Z==0,], sampleW = sampleW,
-            targetW = w_ate_c * sampleWc, std = std)
+  eb_target(sampleXM[Z==1,], targetX = sampleXM[Z==0,], sampleW = sampleW[Z==1],
+            targetW = w_ate_c * sampleW[Z==0], std = std, lambda = lambda)
 }
